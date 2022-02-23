683fc9efc49406b6e34a2e93cb18c3cf479e47eb
Alajandro Martinez <aeropunkrock96@gmail.com>
2022-02-23 09:39:31 -0600

    finishing code

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Assignment code here
+// Assignment code
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var numbers = ['0','1','2','3','4','5','6','7','8','9'];
-var specials = []
+var specials = ['!','@','#','$','^','&','*','(',')','{','}','[',']','|',';',':',',','<','.','>','/','?'];
+var allKeys = [];
 
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
@@ -16,8 +16,66 @@ function generatePassword () {
     window.alert("Your password will be " + askUser + " characters long.");
   }
 
+  hasLowerCase = confirm("Do you want lowercase characters in your password?");
+  if (hasLowerCase) {
+    alert("Your password will have lowercase characters.");
+  }
+  else {
+    alert("Your password will NOT have lowercase characters.");
+  }
 
-}
+  hasUpperCase = confirm("Do you want uppercase characters in your password?");
+  if (hasUpperCase) {
+    alert("Your password will have uppercase characters.");
+  }
+  else {
+    alert("Your password will NOT have uppercase charcters.")
+  }
+
+  hasNumbers = confirm("Do you want numbers characters in your password?");
+  if (hasNumbers) {
+    alert("Your password will have numbers characters.");
+  }
+  else {
+    alert("Your password will NOT have numbers charcters.")
+  }
+
+  hasSpecials = confirm("Do you want special characters in your password?");
+  if (hasSpecials) {
+    alert("Your password will have special characters.");
+  }
+  else {
+    alert("Your password will NOT have special charcters.")
+  }
+
+  if (hasLowerCase === false && hasUpperCase === false && hasNumbers === false && hasSpecials === false) {
+    return"Please select at least one character type.";
+  };
+
+  if (hasLowerCase) {
+    allKeys = allKeys.concat(lowerCase);
+  }
+
+  if (hasUpperCase) {
+    allKeys = allKeys.concat(upperCase);
+  }
+
+  if (hasNumbers) {
+    allKeys = allKeys.concat(numbers);
+  }
+
+  if (hasSpecials) {
+    allKeys = allKeys.concat(specials);
+  }
+
+  let usersNewPassword = ""
+  for (let i = 0; i < askUser; i++) {
+    let rng =[Math.floor(Math.random() * allKeys.length)];
+    usersNewPassword = usersNewPassword + allKeys[rng];
+  }
+
+  return usersNewPassword;
+};
 
 // Write password to the #password input
 function writePassword() {
